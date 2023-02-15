1e4f44410f62c77454e7078793fd6393292b588a
Сергей Макаров <smakarov@MacBook-Pro-Sergej-Makarov-7.local>
2023-02-15 06:23:18 +0300

    update env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,6 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
-  config.action_mailer.delivery_method = :sendmail
 
 
   config.action_mailer.perform_deliveries = true
