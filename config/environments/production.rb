a16ba4ef7c7ef435bcd863d078d93a454faa2df0
Сергей Макаров <smakarov@MacBook-Pro-Sergej-Makarov-7.local>
2023-02-22 06:47:17 +0300

    add host in url options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
-  Rails.application.routes.default_url_options[:host] = 'wwww.archangels.in.ua'
+  Rails.application.routes.default_url_options[:host] = 'archangels.in.ua'
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
