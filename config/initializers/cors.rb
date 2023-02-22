eacf0e210e7db6d64378ab0a0ecd9d723cd4c18b
Сергей Макаров <smakarov@MacBook-Pro-Sergej-Makarov-7.local>
2023-02-22 05:56:48 +0300

    Update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # origins "https://www.archangels.in.ua"
-    origins "*"
+    origins "https://www.archangels.in.ua"
 
     resource "*",
       headers: :any,
