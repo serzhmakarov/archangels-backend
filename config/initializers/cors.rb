98e19b80d7cceeb560d4ade8d7921ed6e8628ae8
Сергей Макаров <makarovserhii@Sergej-Makarovs-MacBook-Pro.local>
2023-09-20 21:11:01 +0300

    Update cors (delete commit)

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
+    origins "https://www.archangels.in.ua"
     resource "*",
       headers: :any,
       expose: ['Authorization'],
